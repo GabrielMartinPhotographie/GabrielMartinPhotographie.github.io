862230cab922eae33681ad9898eb2d7dcee2e366
GabrielMartinPhotographie <160144859+GabrielMartinPhotographie@users.noreply.github.com>
2024-02-18 15:51:31 +0100

    mise à jour

--- portfolio/photogllary/lightbox.js
+++ portfolio/photogllary/lightbox.js
@@ -1,5 +1,3 @@
-// lightbox.js
-
 function openLightboxWithWatermark(imgSrc) {
   var lightbox = document.getElementById('lightbox');
   var lightboxImg = document.getElementById('lightbox-img');
@@ -11,5 +9,22 @@ function openLightboxWithWatermark(imgSrc) {
   // Ajoutez la classe pour appliquer les styles CSS
   lightboxImg.classList.add('lightbox-content');
 
+  // Ajoutez la classe 'initial' pour masquer la lightbox au chargement de la page
+  lightbox.classList.add('initial');
+
   lightbox.style.display = 'flex';
+
+  // Supprimez la classe 'initial' après un court délai pour afficher correctement la lightbox
+  setTimeout(function () {
+      lightbox.classList.remove('initial');
+  }, 100);
+}
+
+function closeLightbox() {
+  var lightbox = document.getElementById('lightbox');
+  var lightboxImg = document.getElementById('lightbox-img');
+
+  lightbox.style.display = 'none';
+  // Ajoutez la classe 'initial' lors de la fermeture pour masquer correctement la lightbox
+  lightbox.classList.add('initial');
 }
